6ac74f2bac154fb941c398d3fff398e87d15f296
Andy Kirkpatrick <ubermonk@gmail.com>
2024-03-06 07:45:25 +0000

    Use Ash.Query.get_argument/2

--- lib/dyndb/gadget/compass.ex
+++ lib/dyndb/gadget/compass.ex
@@ -15,9 +15,11 @@ defmodule Dyndb.Gadget.Compass do
       argument :tenant, :string, allow_nil?: false
 
       prepare fn query, context ->
-        if %{arguments: %{tenant: tenant}} = query do
-          pid = Dyndb.Repo.get_connection!(tenant)
-          Dyndb.DynamicRepoTracer.set_span_context({Dyndb.Repo, pid})
+        case Ash.Query.get_argument(query, :tenant) do
+          tenant when is_binary(tenant) ->
+            pid = Dyndb.Repo.get_connection!(tenant)
+            Dyndb.DynamicRepoTracer.set_span_context({Dyndb.Repo, pid})
+          _ -> :ok
         end
 
         query
